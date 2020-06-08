6b318e91fbbb52f111b307037ac4c4178668fd00
Ian Gregg <iangregg188@gmail.com>
2020-06-08 10:34:45 -0700

    practiced cloneing arr1 & arr2 then combining

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,23 +1,35 @@
-const num1 = [3,2,1]
+let array = [3,2,1]
 
-function stringNum(num1)({
-let i=0
-(var= i; i<=num1.length; i++)
-  if (i=1){
-    num1.push(4)
-  }
-  if(i=0){
-    num1.Pop(1)
-  }
-  if(i=2){
-    num1.shift(0)
-  }
-  else{
-    num1.unshift(2)
-  }
+array.push(4)
+array.pop(2)
+array.shift(0)
+array.unshift(2)
+
+const arr1 = ["thor", "spiderman", "antman", "batman"]
+const arr2 = ["marcy", "Theo", "Norma", "Fredrick"]
+
+let clonearr1 = arr1.cocat(arr2);
+
+//array[0];
+
+//function stringNum(num1)({
+//let i=0
+//(var= i; i<=num1.length; i++)
+  //if (i=1){
+    //num1.push(4)
+  //}
+  //if(i=0){
+    //num1.Pop(1)
+  //}
+  //if(i=2){
+    //num1.shift(0)
+  //}
+  //else{
+    //num1.unshift(2)
+  //}
 
 
 
-});
+//});
 
-return
+//return
